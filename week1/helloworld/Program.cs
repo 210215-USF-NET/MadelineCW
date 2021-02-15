488c7386244ca3c5ee9185c2a29c9e0bbad08840
madcyn <35504047+madcyn@users.noreply.github.com>
2021-02-15 13:57:25 -0800

    added week1 for organization

---
+++ week1/helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names={"mads","madeline","maddy","Madzy","Ms Williams","hey you","pretty lady","rockstar","crazy woman", "friend","happy cool"};
+            for(int i=0;i<10;++i){
+                Random r = new Random();
+                int gr= r.Next(0,names.Length);
+            Console.WriteLine($"Hello {names[gr]}!");
+            }
+            //string name = Console.ReadLine();
+           // Console.WriteLine($"hi {name}");
+        }
+    }
+}
